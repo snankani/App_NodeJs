75584b7850de79144715cb6dad356cc7a996f609
Gopinath C H <gopinathch@gopimac.guest.corp.microsoft.com>
2019-01-26 15:01:35 +0530

    updated app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Docker Sample!\n');
+  res.send('Hello World from Kubernetes CICD !\n');
 });
 
 app.listen(PORT, HOST);
